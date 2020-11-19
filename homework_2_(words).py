21f040b8b8c9afcc8aab11013f1dfa268fb6b67d
kate696 <62883922+kate696@users.noreply.github.com>
2020-11-19 20:54:25 +0100

    Add files via upload

---
+++ homework_2_(words).py
@@ -0,0 +1,38 @@
+
+import sys
+
+txt = open("article.txt", "r")
+txt1 =(txt.read().lower())
+
+to_remove = '''!()-[]{};:'"\,<>./?@#$%^&*_~â€™””'''
+
+words = ""
+for char in txt1:
+   if char not in to_remove:
+      words = words + char
+w=words.split()
+
+def word_count(str):
+    counts = dict()
+    for word in w:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+print(word_count(w))
+
+
+
+
+
+
+
+
+
+
+
+
+
